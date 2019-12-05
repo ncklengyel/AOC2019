2f9e15500f8774a1f589541ef7d7d1bbb5a986dd
Nicolas Lengyel <ncklengyel@gmail.com>
2019-12-04 23:50:19 -0500

    AOC2 part 2

--- AOC2/AOC2.cpp
+++ AOC2/AOC2.cpp
@@ -35,25 +35,43 @@ void process(int code, int input1, int input2, int output, std::vector<int> &vec
     vect.at(output) = result;
 }
 
-void run(const std::vector<std::string> &&vect, std::vector<int> &outVect)
+void run(const std::vector<int> &vect, std::vector<int> &outVect, int pNoun, int pVerb)
 {
-    std::vector<int> vectInt;
-    for (auto num : vect)
-    {
-        vectInt.push_back(std::stoi(num));
-    }
+    outVect = vect;
 
-    vectInt.at(1) = 12;
-    vectInt.at(2) = 2;
+    outVect.at(1) = pNoun;
+    outVect.at(2) = pVerb;
 
     int currentIndex = 0;
-    while (vectInt.at(currentIndex) != CODE::STOP)
+    while (outVect.at(currentIndex) != CODE::STOP)
     {
-        process(vectInt.at(currentIndex), vectInt.at(currentIndex + 1), vectInt.at(currentIndex + 2), vectInt.at(currentIndex + 3), vectInt);
+        process(outVect.at(currentIndex), outVect.at(currentIndex + 1), outVect.at(currentIndex + 2), outVect.at(currentIndex + 3), outVect);
         currentIndex += 4;
     }
+}
 
-    outVect = vectInt;
+std::pair<int,int> run2(const std::vector<int>& vect, std::vector<int>& outVect, int pExpected)
+{
+    std::pair<int, int> result;
+
+    for (int i = 0; i < 99; i++)
+    {
+        for (int j = 0; j < 99; j++)
+        {
+            std::vector<int> tempVect;
+            run(vect, tempVect, i, j);
+
+            if(tempVect.at(0) == pExpected)
+            {
+                outVect = tempVect;
+                result.first = i;
+                result.second = j;
+                break;
+            }
+        }   
+    }
+
+    return result;
 }
 
 int main()
@@ -61,7 +79,6 @@ int main()
     std::ifstream file("./input.txt");
     std::string line;
     std::string data;
-    const char delim = ',';
 
     while (std::getline(file, line))
     {
@@ -71,10 +88,23 @@ int main()
     std::vector<std::string> vect;
     boost::split(vect, data, boost::is_any_of(","));
 
+    std::vector<int> vectInt;
+    for (auto num : vect)
+    {
+        vectInt.push_back(std::stoi(num));
+    }
+
     std::vector<int> result;
-    run(std::move(vect), result);
+    std::vector<int> result2;
 
-    std::cout << "Result: " << result.at(0) << std::endl;
+    run(vectInt, result, 12, 2);
+    auto nounVerb = run2(vectInt, result2, 19690720);
 
+    std::cout << "Result1: " << result.at(0) << std::endl;
+    
+    int answer2 = 100 * nounVerb.first + nounVerb.second;
+
+    std::cout << "Result2: noun= " << nounVerb.first << " verb= "<< nounVerb.second << " total= " << answer2 << std::endl;
+    
     return 0;
 }
